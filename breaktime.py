f838c0b13c511a584db7993083a62271a77c7544
rosiehenrytech373 <Rosiea28@gmail.com>
2026-05-25 20:20:38 -0500

    Add files via upload
    Take a Break Python wellness app

---
+++ breaktime.py
@@ -0,0 +1,13 @@
+import time
+import webbrowser
+
+total_breaks = 3
+break_count = 0
+
+print("This program started on " + time.ctime())
+while break_count < total_breaks:
+    time.sleep(10)
+    webbrowser.open("https://youtu.be/01OtiIU_eZ4")
+    break_count = break_count + 1
+
+print("All breaks completed. Good job taking care of yourself!")
